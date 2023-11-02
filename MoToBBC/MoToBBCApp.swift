9239b5ee5b96238af23a22d9447a6d1aeddbe12f
Sawaki Shuto <swkshuto0208@icloud.com>
2023-11-03 02:22:17 +0900

    admob初期設定完了

--- MoToBBC/MoToBBCApp.swift
+++ MoToBBC/MoToBBCApp.swift
@@ -8,9 +8,11 @@
 import SwiftUI
 import FirebaseCore
 import FirebaseAuth
+import GoogleMobileAds
 class AppDelegate: NSObject, UIApplicationDelegate {
 func application(_ application: UIApplication,
                 didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+    GADMobileAds.sharedInstance().start(completionHandler: nil)
 
     return true
 }
