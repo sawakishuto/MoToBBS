7e6ef15f5090d1c2e65c2dd553deebe32f28349e
Sawaki Shuto <swkshuto0208@icloud.com>
2023-11-11 00:12:21 +0900

    LoginView不要になった変数削除

--- MoToBBC/ScreenView/LoginView.swift
+++ MoToBBC/ScreenView/LoginView.swift
@@ -3,40 +3,9 @@ import FirebaseAuth
 import CoreData
 // swiftlint:disable line_length
 struct LoginView: View {
-    @ObservedObject private var viewModel = LoginViewModel()
-    @Environment(\.managedObjectContext) private var viewContext
-    @FetchRequest(
-        entity: LoginInfo.entity(),
-        sortDescriptors: [NSSortDescriptor(key: "pass", ascending: false)],
-        animation: .default
-    ) var fetchedInfo: FetchedResults<LoginInfo>
-    @State private var checkerror: Bool = false
-    @State var showsheet = false
-    @State var showconfine = false
-    @State public var usersname: String = ""
-    @State public var bikename: String = ""
-    @State public var usercomment: String = ""
     @State var logingo = true
-    @State var eventid: String = ""
-    @State var userid: String = ""
-    @State public var mail: String = ""
-    @State public var password: String = ""
-    @State public var errorMessage: String = ""
-    @State var profile = false
-    @State var check = false
-    @State  var checkms = false
     @State private var allview: Bool = false
-    @State private var errorhandle: Bool = false
-    @State var checkname = "checkmark.circle"
-    @State private var mailname: String = " MoToBBS@gmail.com"
-    @State private var passname: String = " 123456"
-    @State private var male: Bool = false
-    @State private var female: Bool = false
-    @State private var andSoOn: Bool = false
-    @State private var mailnames: String = "MoToBBS@email.com"
-    @State private var passnames: String = "123456"
-    @State private var authState:String = ""
-    @State private var progresState:String = "新規登録"
+
 
 
     var body: some View {
