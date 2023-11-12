01a0417422ebeb8bb41d8ad76dcb66772159f0ce
Sawaki Shuto <swkshuto0208@icloud.com>
2023-11-12 21:57:23 +0900

    TutorialView以下

---
+++ MoToBBC/ScreenView/TutorialView.swift
@@ -0,0 +1,18 @@
+//
+//  TutorialView.swift
+//  MoToBBS
+//
+//  Created by 澤木柊斗 on 2023/11/12.
+//
+
+import SwiftUI
+
+struct TutorialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TutorialView()
+}
