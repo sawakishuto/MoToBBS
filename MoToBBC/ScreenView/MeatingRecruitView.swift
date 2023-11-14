589dd38ea9adec4920189d2666d5b249c66ffdb8
Sawaki Shuto <swkshuto0208@icloud.com>
2023-11-14 17:18:03 +0900

    MeatingRecruitView追加

---
+++ MoToBBC/ScreenView/MeatingRecruitView.swift
@@ -0,0 +1,18 @@
+//
+//  MeatingRecruitView.swift
+//  MoToBBS
+//
+//  Created by 澤木柊斗 on 2023/11/14.
+//
+
+import SwiftUI
+
+struct MeatingRecruitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeatingRecruitView()
+}
