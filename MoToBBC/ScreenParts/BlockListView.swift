3a57a0fddd68ca5d0a808d35726a183ffac9b605
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-06 00:36:18 +0900

    削除
    BlockListViewの不要コード削除

--- MoToBBC/ScreenParts/BlockListView.swift
+++ MoToBBC/ScreenParts/BlockListView.swift
@@ -2,7 +2,6 @@ import SwiftUI
 import CoreData
 
 struct BlockListView: View {
-    @ObservedObject private var viewModel = ViewModels()
     @Environment(\.managedObjectContext) private var viewContext
     @FetchRequest(
         entity: BlockList.entity(),
