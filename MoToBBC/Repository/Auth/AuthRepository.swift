ad53d94c7f6498d19876a149f5b98631a0830c55
Sawaki Shuto <swkshuto0208@icloud.com>
2024-05-23 01:51:56 +0900

    feat: 一旦Repository実装

--- MoToBBC/Repository/Auth/AuthRepository.swift
+++ MoToBBC/Repository/Auth/AuthRepository.swift
@@ -6,3 +6,70 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseAuth
+import Combine
+
+final class AuthRepository: AuthRepositoryProtocol {
+    func deleteAccount(uid: String) -> AnyPublisher<Bool, Error> {
+        return Deferred {
+            Future { promise in
+                guard let user = self.auth.currentUser else {return}
+                user.delete { error in
+                    if let error = error {
+                        promise(.failure(error))
+                    } else {
+                        promise(.success(true))
+                    }
+
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func signUp(mailAdress: String, password: String) -> AnyPublisher<AuthDataResult, Error> {
+        return Deferred {
+            Future { promise in
+                self.auth.createUser(withEmail: mailAdress, password: password) { result, error in
+                    if let error = error {
+                        promise(.failure(error))
+                    } else if let result = result {
+                        promise(.success(result))
+                    }
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func login(mailAdress: String, password: String) -> AnyPublisher<AuthDataResult, Error> {
+        return Deferred {
+            Future { promise in
+                self.auth.signIn(withEmail: mailAdress, password: password) { result, error in
+                    if let error = error {
+                        promise(.failure(error))
+                    } else if let result = result {
+                        promise(.success(result))
+                    }
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func resetPassword(mailAdress: String) {
+        auth.sendPasswordReset(withEmail: mailAdress) { error in
+            if let error = error {
+                print(error)
+            } else {
+                print("success!")
+            }
+        }
+
+    }
+    
+    let auth = Auth.auth()
+
+
+}
